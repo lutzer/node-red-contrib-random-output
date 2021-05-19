d29a1fad5fad243d2f95f1684c40aa620887a793
dcs0spb <s.p.bradley@durham.ac.uk>
2021-05-19 09:13:03 +0100

    Added weighted output functionality

--- random-output.js
+++ random-output.js
@@ -2,12 +2,37 @@ module.exports = function(RED) {
   function RandomOutputNode(config) {
     RED.nodes.createNode(this, config)
       let node = this;
+      node.weights = [];
+      for(let weight of config.weights){
+        weight = Number(weight);
+        if(isNaN(weight) || weight < 0){
+          weight = 1;
+        }
+        node.weights.push(weight);
+      }
 
       node.on('input', function(msg) {
         const numberOfOutputs = node.wires.length
-        
         let output = new Array(numberOfOutputs);
-        output[Math.floor(Math.random()*numberOfOutputs)] = msg;
+
+        let weightSum = node.weights.reduce((a, b) => a + b, 0);
+        if(weightSum <= 0){
+          node.weights.fill(0, 1);
+          weightSum = numberOfOutputs;
+        }
+
+        const randVal = Math.random() * weightSum;
+        let weightAggregate = 0;
+        let chosen;
+        for(let outputNum = 0; outputNum < numberOfOutputs; outputNum++){
+          weightAggregate += node.weights[outputNum];
+          if(randVal < weightAggregate){
+            chosen = outputNum;
+            break;
+          }
+        }
+
+        output[chosen] = msg;
         node.send(output)
       });
   }
